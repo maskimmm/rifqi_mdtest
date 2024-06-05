78200396ca7163ee7674f4efb9a02790bcfadff7
maskimmm <rifqi231@gmail.com>
2024-06-05 16:44:52 +0700

    Initial Commit

---
+++ mdtest/mdtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  mdtestApp.swift
+//  mdtest
+//
+//  Created by Rifqi Alhakim Hariyantoputera on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct mdtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
